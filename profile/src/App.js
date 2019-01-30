57abaa4d086ca4f35cc9d1668ec266173c0941f2
juhee <sjh5665@gmail.com>
2019-01-30 22:33:05 +0900

    [ADD] : append camera

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import profileImg from './profileImg.png';
+import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import './App.css';
+
+const styles = {
+  mainTitle: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  leftCategory: {
+    flex: 0.2,
+    backgroundColor: 'white',
+    height: 100,
+  },
+  navbar: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#282c34',
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={profileImg} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+       <div> 
+         <p> JuheeUU</p>
+       </div>
     );
   }
 }
